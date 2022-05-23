d5332b9a7d0058207b93feb994d00ccab6ee943f
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2022-05-23 11:22:40 +0100

    Update _printf.c
    edited

--- _printf.c
+++ _printf.c
@@ -1,48 +1,50 @@
 #include "main.h"
-
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - imitates printf in the stdio.h
+ * @format: strings to format
+ * Return: Always 0
  */
-int _printf(const char *format, ...)
+
+int _printf(char *format, ...)
 {
-    int (*pfunc)(va_list, flags_t *);
-    const char *p;
-    va_list arguments;
-    flags_t flags = {0, 0, 0};
+	char *transcript, *s;
+	unsigned int i;
+	int number_count = -1;
+	va_list args;
+	va_start(args, format);
 
-    register int count = 0;
+	for (transcript = format; *transcript != '\0'; *transcript++)
+	{
+		while (*transcript != '%')
+		{
+			_putchar(*transcript);
+			number_count++, transcript++;
+		}
+		transcript++;
 
-    va_start(arguments, format);
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-    for (p = format; *p; p++)
-    {
-        if (*p == '%')
-        {
-            p++;
-            if (*p == '%')
-            {
-                count += _putchar('%');
-                continue;
-            }
-            while (get_flag(*p, &flags))
-                p++;
-            pfunc = get_print(*p);
-            count += (pfunc)
-                         ? pfunc(arguments, &flags)
-                         : _printf("%%%c", *p);
-        }
-        else
-            count += _putchar(*p);
-    }
-    _putchar(-1);
-    va_end(arguments);
-    return (count);
+		switch(*transcript)
+	{
+		case 's': s = va_arg(args, char *);
+					print_string(s);
+					number_count++;
+					break;
+		case '%': s = va_arg(args, char *);
+					_putchar(*s + 0);
+					number_count++;
+					break; 
+		case 'd': i = va_arg(args, unsigned int);
+				print_integer(i);
+				number_count += print_integer(i);
+				break;
+		case 'c': s = va_arg(args, char *);
+				_putchar('%');
+				number_count++;
+				break;
+		case 'i': i = va_arg(args, unsigned int);
+				print_integer(i);
+				break;
+	}
+	va_end(args);
+	return (number_count);
+	}
 }
